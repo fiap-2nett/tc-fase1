89c44b88e747ee82a0a195bc5c2b30097ffd87e2
PauloFelipeN <paulo_felipe06@hotmail.com>
2023-09-30 12:34:33 -0300

    fix(seed): ajustado a role de user para general

--- src/TechChallenge.Persistence/Migrations/20231002222921_Fix_UserRoles.Designer.cs
+++ src/TechChallenge.Persistence/Migrations/20231002222921_Fix_UserRoles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechChallenge.Persistence;
 
@@ -11,9 +12,11 @@ using TechChallenge.Persistence;
 namespace TechChallenge.Persistence.Migrations
 {
     [DbContext(typeof(EFContext))]
-    partial class EFContextModelSnapshot : ModelSnapshot
+    [Migration("20231002222921_Fix_UserRoles")]
+    partial class Fix_UserRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TechChallenge.Persistence/Migrations/20231002222921_Fix_UserRoles.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechChallenge.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_UserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: (byte)2,
+                column: "Name",
+                value: "Geral");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: (byte)2,
+                column: "Name",
+                value: "Usuário");
+        }
+    }
+}
