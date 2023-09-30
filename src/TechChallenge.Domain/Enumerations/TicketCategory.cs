4e2ac3a3177f0d8ca11419237845bf812a28f03f
PauloFelipeN <paulo_felipe06@hotmail.com>
2023-09-30 12:26:28 -0300

    criado o enumerations ticketCategoria

---
+++ src/TechChallenge.Domain/Enumerations/TicketCategory.cs
@@ -0,0 +1,10 @@
+namespace TechChallenge.Domain.Enumerations
+{
+    public enum TicketCategory : int
+    {
+        Unavailability = 1,
+        Slowness = 2,
+        Request = 3,
+        Doubt = 4,
+    }
+}
